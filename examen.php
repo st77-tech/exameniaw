637bf504609c9dbae7f9bc685f688aacced45ac7
SM <sm@gmail.com>
2024-10-31 08:48:55 +0000

    secundo commit

--- examen.php
+++ examen.php
@@ -1,2 +1,15 @@
-<?php echo 'steve marin -31/10/24 '?>
-<?php echo 'una carpeta de fichero '?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1><?php echo 'steve marin'?></h1>
+<h2><?php echo '31/10/24'?></h2>
+<h3><?php echo 'una carpeta de fichero '?></h3> 
+</body>
+</html>
+
+
