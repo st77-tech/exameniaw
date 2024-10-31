803414e511a169c22a27775e459d7cb4e0cfed41
SM <sm@gmail.com>
2024-10-31 08:58:37 +0000

    tercer commit

--- examen.php
+++ examen.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+    <div class="message">
 <h1><?php echo 'steve marin'?></h1>
 <h2><?php echo '31/10/24'?></h2>
 <h3><?php echo 'una carpeta de fichero '?></h3> 
+</div>
 </body>
 </html>
 
